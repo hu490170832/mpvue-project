ad10e449e76cfcc60963de40dc613152cf35bd6f
hucheng <hucheng@zsyjr.com>
2018-06-26 11:05:12 +0800

    网络请求加入toastLoading提示

--- src/api/config.js
+++ src/api/config.js
@@ -1,17 +1,22 @@
 import { resolve } from "path";
 
-export const host = 'http://192.168.0.122:5757'
+export const host = 'http://172.16.18.35:5757'
 // export const host = 'https://s35dsezw.qcloud.la'
 export const ERR_OK = 0
 
 export function get(url,data={}) {
     return new Promise((resolve,reject)=>{
+        wx.showLoading({
+            title: '加载中',
+            mask:true
+        })
         wx.request({
             url:host+url,
             data,
             method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
             // header: {}, // 设置请求的 header
             success: function(res) {
+                wx.hideLoading()
                 if(res.data.code == ERR_OK) {
                     resolve(res.data)
                 }else {
@@ -29,6 +34,7 @@ export function get(url,data={}) {
             }
         })
     }).catch(err=>{
+        wx.hideLoading()
         wx.showToast({
             title: '网络错误',
             icon: 'none',
@@ -40,12 +46,17 @@ export function get(url,data={}) {
 
 export function post(url,data={}) {
     return new Promise((resolve,reject)=>{
+        wx.showLoading({
+            title: '加载中',
+            mask:true
+        })
         wx.request({
             url:host+url,
             data,
             method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
             // header: {}, // 设置请求的 header
             success: function(res) {
+                wx.hideLoading()
                 if(res.data.code ==ERR_OK) {
                     resolve(res.data)
                 }else {
@@ -63,6 +74,7 @@ export function post(url,data={}) {
             }
         })
     }).catch(err=>{
+        wx.hideLoading()
         wx.showToast({
             title: '网络错误',
             icon: 'none',
